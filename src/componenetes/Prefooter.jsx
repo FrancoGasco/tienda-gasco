3bcb579d191f8aff4f4b4812a73cbb1492c349ea
FrancoGasco <francogasco02@gmail.com>
2022-09-05 16:11:44 -0300

    contador con boton

--- src/componenetes/Prefooter.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Prefooter = () => {
-    return(
-        <p><strong>Coderhouse </strong></p>
-    )
-}
--- src/containers/ItemListContainer.jsx
+++ src/containers/ItemListContainer.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { ItemCount } from "../components/ItemCount";
 import './ItemListContainer.css'
 
 
 export const ItemListContainer = ({greeting}) =>{
+    
+    const handleAdd = () =>{
+        console.log("Se agrego al carrito")
+    }
+
     return(
+        <>
+        
         <div className="greeting">{greeting}</div>
+        
+        <ItemCount handleAdd={handleAdd} initialStock={10}/>
+        
+        </>
     )
 }
